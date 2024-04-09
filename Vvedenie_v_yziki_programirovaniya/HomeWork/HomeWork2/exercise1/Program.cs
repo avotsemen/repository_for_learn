274980e43862cb8924a32bd7bb087b51a6987d02
Mamaev Semyon <avot.semen@gmail.com>
2024-04-10 00:10:44 +0500

    Добавил решение Задачи 1 к семинару за 08.04.2024

---
+++ Vvedenie_v_yziki_programirovaniya/HomeWork/HomeWork2/exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, 
+//которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+void CheckMultiplicity(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine($"число {num} кратно числу 7 и 23");
+    }
+    else
+    {
+        System.Console.WriteLine($"число {num} не кратно числу 7 и 23");
+    }
+}
+
+Console.Write("Введите число: ");//выводим в консоль сообщение
+int num = Convert.ToInt32(Console.ReadLine());//запрос ввода цифры
+
+//System.Console.WriteLine(DeleteSecondDigit(num));
+CheckMultiplicity(num);
