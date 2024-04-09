390eb92f28550b296a381933944e76d71bf1bef6
Mamaev Semyon <avot.semen@gmail.com>
2024-04-10 00:47:01 +0500

    Добавил решение задачи 3 к семинару за 08ю04ю2024

---
+++ Vvedenie_v_yziki_programirovaniya/HomeWork/HomeWork2/exercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает 
+// на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+void FindMaxDigit(int n)
+{
+
+    if (9 < n && n < 100)
+    {
+        int ed = n % 10;
+        int des = n / 10;
+        if (ed >= des)
+        {
+            System.Console.WriteLine(ed);
+        }
+        else 
+        {
+            System.Console.WriteLine(des);
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не двухзначное число");
+    }
+}
+
+Console.Write("Введите двухзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+FindMaxDigit(n);
