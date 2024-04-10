18146e2d584fbcb56375c1159383d17c4092a72a
Mamaev Semyon <avot.semen@gmail.com>
2024-04-10 23:53:52 +0500

    Добавил решение задачи 4 к семинару за 08.04.2024

---
+++ Vvedenie_v_yziki_programirovaniya/HomeWork/HomeWork2/exercise4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая на вход 
+// принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+void Main(int n)
+{
+
+    if (n > 0)
+    {
+        int divider = 10;
+        while (divider <= n )
+        {
+            divider*=10;
+        }
+        divider /=10;
+        int currentDigit = 0;
+        while (divider >= 10)
+        {
+            currentDigit = n / divider;
+            n = n % divider;
+            System.Console.Write($"{currentDigit}, ");
+            divider /= 10;
+        }
+        currentDigit = n / divider;
+        System.Console.Write($"{currentDigit}");
+    }
+    else
+    {
+        System.Console.WriteLine("Введите значение больше 0.");
+        n = Convert.ToInt32(Console.ReadLine());
+        Main(n);
+    }
+}
+
+
+Console.Write("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Main(n);
