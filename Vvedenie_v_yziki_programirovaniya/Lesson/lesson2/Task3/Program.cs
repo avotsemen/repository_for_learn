50d1f1e2bc77083e5f14aca242b1d9b87d010826
Mamaev Semyon <avot.semen@gmail.com>
2024-04-13 21:43:59 +0500

    Added Task3:lesson2

---
+++ Vvedenie_v_yziki_programirovaniya/Lesson/lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 2, 2, 8, 6, 4, 8, 9, 28, 252, 2 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i++;
+}
