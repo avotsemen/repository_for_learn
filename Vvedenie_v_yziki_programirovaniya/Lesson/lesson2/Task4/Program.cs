8fed6a4e02e6c9390b0a0b1caffd5cca9e8629fe
Mamaev Semyon <avot.semen@gmail.com>
2024-04-13 21:44:11 +0500

    Added Task4:lesson2

---
+++ Vvedenie_v_yziki_programirovaniya/Lesson/lesson2/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿//int n = 5;
+int[] array = { 59, 6, 4, 5, 58 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
