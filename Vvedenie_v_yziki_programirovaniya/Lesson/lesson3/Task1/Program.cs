642e7e51abfd9735902fb0088690d25183c09313
Mamaev Semyon <avot.semen@gmail.com>
2024-04-23 20:51:22 +0500

    added lesson3:task1

---
+++ Vvedenie_v_yziki_programirovaniya/Lesson/lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalcuateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomerator = c + d;
+    double result = numenator / denomerator;
+    return result;
+}
+
+double result = CalcuateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
