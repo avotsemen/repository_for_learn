79589d1d6e0211ae5afb2870a2902c119ababe2f
Mamaev Semyon <avot.semen@gmail.com>
2024-04-23 21:34:16 +0500

    Added lesson3:Task3

---
+++ Vvedenie_v_yziki_programirovaniya/Lesson/lesson3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 8, 2, 3, 4, 6 };
+ZeroEvenElements(array);
+PrintArray(array);
