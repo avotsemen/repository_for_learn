a9ec03fe86dfb12165dfb96abc0cdb87510dfea0
Mamaev Semyon <avot.semen@gmail.com>
2024-04-23 20:59:46 +0500

    Added lesson3:Task2

---
+++ Vvedenie_v_yziki_programirovaniya/Lesson/lesson3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquares(int limit)
+{
+    int i =1;
+    while (i<=limit)
+    {
+        System.Console.Write($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+System.Console.WriteLine();
+PrintSquares(10);
+System.Console.WriteLine();
+PrintSquares(15);
