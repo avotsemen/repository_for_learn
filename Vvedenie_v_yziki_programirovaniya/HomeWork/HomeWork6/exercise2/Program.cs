e2bfcb05d6895002bc3378304afc0e8493d11736
Mamaev Semyon <avot.semen@gmail.com>
2024-04-24 22:00:25 +0500

    Добавил решение задачи 2 к семинару 6 'Массивы и строки'

---
+++ Vvedenie_v_yziki_programirovaniya/HomeWork/HomeWork6/exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string CreateNewRandomString(int length) //Функция создания случайной строки длиной length
+{
+    Random rnd = new Random();
+    string str = string.Empty;
+    string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+    for (int i = 0; i < length; i++)
+    {
+        str = str + chars[rnd.Next(0, chars.Length)];
+    }
+    return str;
+}
+
+string GetLettersUpToDown(string str) //Функция понижения регистра
+{
+    string strLow = str.ToLower();
+    return strLow;
+}
+
+System.Console.WriteLine("Введите длину строки: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string temp = CreateNewRandomString(length);
+System.Console.WriteLine(temp);
+System.Console.WriteLine(GetLettersUpToDown(temp));
