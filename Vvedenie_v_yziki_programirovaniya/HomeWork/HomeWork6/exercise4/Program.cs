bff06304692a132b1b4df0db85274957e9aa43f9
Mamaev Semyon <avot.semen@gmail.com>
2024-04-30 22:02:53 +0500

    Добавил решение задачи 4 к семинару 6 'Массивы и строки'

---
+++ Vvedenie_v_yziki_programirovaniya/HomeWork/HomeWork6/exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16"); // Устанавливаем кодировку ввода на UTF-16
+        Console.WriteLine("Введите строку из слов, разделенных пробелами:");
+        string input = Console.ReadLine();
+
+        string reversedString = ReverseWords(input);
+        
+        Console.WriteLine("Строка c обратным порядком слов:");
+        Console.WriteLine(reversedString);
+    }
+
+    static string ReverseWords(string str)
+    {
+        // Разбиваем строку на слова
+        string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Обратный порядок слов
+        Array.Reverse(words);
+
+        // Объединяем слова обратно в строку с пробелами
+        return string.Join(" ", words);
+    }
+}
